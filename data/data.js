d295746a8097d3d1d057496ac49a8bf418c26a5c
pankajgouraw <pgouraw@gmail.com>
2019-12-05 15:51:16 +0530

    changes add place value and showerror

--- data/data.js
+++ data/data.js
@@ -1,11 +1,15 @@
 
-const headerText = 'Four digit addition';
-const headerInstructionColor = 'white';
-const minA = 10;
-const maxA = 90;
-const minB = 10;
-const maxB = 90;
-const bg = 'radial-gradient(#4CAF50, #009688)';
+const headerText = '5-Digit Addition without Carry';
+const headerInstructionColor = '#000';
+const questColor = "rgb(43, 14, 251)";
+const ansColor = "hsl(262, 52%, 47%)";
+const numColor = '#000';
+const errorColor = 'red';
+const minA = 10000;
+const maxA = 90000;
+const minB = 1000;
+const maxB = 9000;
+const bg = 'linear-gradient(#edeeee,rgb(179, 156, 156))';
 
 
 
--- js/script.js
+++ js/script.js
@@ -6,9 +6,12 @@ $(function() {
     $("#headerText").text(headerText);
     $("#instruction").css({color:headerInstructionColor});
     $('body').css({'background-image':bg});
+    $('#firstNo, #secNo, .sign').css({color:questColor});
+     $('.options p span').css({color:numColor});
+    // $('.drop span').css({color:ansColor});
     generateContent();
     dragDrop();
-
+   // $('.drop span').css({color:ansColor});
     // function for drag and drop
   function dragDrop(){
 
@@ -22,8 +25,10 @@ $(function() {
             accept:".drag",
             // tolerance: 'intersect',
             drop: function(event, ui) {
+               // $('.drop span').css({color:ansColor});
 
-             $(this).append($(ui.draggable).clone());
+             $(this).append($(ui.draggable).clone().css({color:ansColor}));
+             $(event.target).attr('data-user',ui.draggable.text());
 
              if($(this).children("span").length > 1){
                 $(this).children("span:nth-child(1)").remove(); 
@@ -34,55 +39,67 @@ $(function() {
   }  //end here drag and drop 
 
   function generateContent(){
+        let rand1 = '';
+        let rand2 = '';
         // generate random numbers
         let randA = Math.ceil(Math.random() * (maxA - minA)+1) + minA;
         let randB = Math.ceil(Math.random() * (maxB - minB)+1) + minB;
 
+
+        let arrayA = Array.from(randA.toString(), Number);
+        let arrayB = Array.from(randB.toString(), Number);
+      
+
+        for(let i=0; i<arrayA.length; i++){
+          let x = Math.ceil(Math.random() * (5));
+          rand1 +=x
+        }
+
+        for(let i=0; i<arrayB.length; i++){
+          let x = Math.ceil(Math.random() * (4));
+          rand2 +=x
+        }
+
+        console.log('rand1', rand1)
+        console.log('rand2', rand2)
+
+        randA = Number(rand1);
+        randB= Number(rand2);
+
+
+
+
         // convert random number into array
         carryRandA = Array.from(randA.toString(), Number);
         carryRandB = Array.from(randB.toString(), Number);
-
-        //generate span tag for numbers
-        carrySpanA = '';
-        carrySpanB = '';
-        
-
+        let carrySpanA = '';
+        let carrySpanB = ''
         //add span tag to the number
-        $.each(carryRandA, function(i,value){
+        $.each(carryRandA, function(i, value) {
             var spanA = `<span>${value}</span>`
             carrySpanA += spanA;
-        }); 
+        });
+
+        // add span tag to second number
+        $.each(carryRandB, function(i, value) {
+            var spanB = `<span>${value}</span>`
+            carrySpanB += spanB;
+        });
 
         $('#firstNo').html(carrySpanA);
-        $('#secNo').html(randB);
+        $('#secNo').html(carrySpanB);
 
         // append carried value
         result = randA + randB;
-
-        // console.log('random A ', carryRandA)
-        // console.log('random B ', carryRandB)
-
-
-        // append the carried value
-        for(let i=carryRandB.length-1; i>=0; i--){
-          let sum =  carryRandA[i] + carryRandB[i];
-          if(sum >9){
-                    let x =$('#firstNo span')[i];
-                    $(x).append('<span>1</span>');
-          } 
-          //console.log('sum : ',sum)
-        }
-
-
-
+        resultArray = Array.from(result.toString(), Number);
         // generate drop box 
         let dropTag='';
-        for(let i = 0; i<result.toString().length; i++){
-           let pTag = '<p class="drop"></p>';
+        for(let i = 0; i<resultArray.length; i++){
+           let pTag = `<p class="drop" data-original="${resultArray[i]}" data-user=' '></p>`;
            dropTag  += pTag;
         }
         $('.ansContainer').html(dropTag);
-  }
+  } //end generatecontent function
 
 
   $('#next').click(function(){
@@ -92,6 +109,10 @@ $(function() {
      $('#showAns').hide();
      generateContent();
      dragDrop();
+      let Width = $('.drop').width();
+      $('#firstNo span').width(Width);
+      $('#secNo span').width(Width);
+      generatePlaceValue();
   });
 
   $('#reload').click(function(){
@@ -102,6 +123,7 @@ $(function() {
  
   $('#check').click(function(){
     // console.log('chance', chance)
+    showError()
      let dropTag = $('.ansContainer p');
      let userInput = '';
      $.each(dropTag , function(i, value){
@@ -111,9 +133,9 @@ $(function() {
      // console.log(parseInt(userInput));
      let output = $('.output');
      // console.log(output)
-     if(userInput == ''){
-        return false;
-     } 
+       if(userInput == ''){
+          return false;
+        }
        $(this).show();
       // $('#next').fadeIn();
       
@@ -131,7 +153,7 @@ $(function() {
         if(chance==0){
           oopsTryAgain();
           chance++;
-          $('.ansContainer .drop').empty();
+          
           return false;
         }
         
@@ -146,7 +168,28 @@ $(function() {
         $('#next').hide();
         $('#reload').fadeIn();
      }
+
+     
+
+     // showError();
+
   })
+
+
+function showError(){
+  let dataAttr = $('.drop');
+  console.log(dataAttr)
+  $.each(dataAttr, function(index, value){
+    let dataUser = $(value).attr('data-user');
+    let dataOriginal = $(value).attr('data-original');
+    if(dataUser == dataOriginal){
+      $(value).css({'borderColor':'#fff'})
+    }else{
+      $(value).css({'borderColor':errorColor})
+    }
+
+  })
+}
   
 
   function oopsTryAgain(){
@@ -158,6 +201,7 @@ $(function() {
         $('.oops').addClass('zoomOut')
         setTimeout(function(){
         $('.oops').removeClass('oopsHW');
+        // $('.ansContainer .drop').empty();
         },500);
       },2000)
   }
@@ -177,17 +221,41 @@ function wellDone(){
 
 
   $('#showAns').click(function(){
-     //console.log(result);
     // generate answer
+    $(this).hide();
         let dropTag = '';
         let ansArray = Array.from(result.toString(), Number);
         // console.log('result',result);
         // console.log('result',ansArray);
         for(let i = 0; i<ansArray.length; i++){
-           let pTag = `<p class="drop"><span>${ansArray[i]}</span></p>`;
+           let pTag = `<p class="drop"><span style='color:${ansColor}'>${ansArray[i]}</span></p>`;
            dropTag  += pTag;
         }
         $('.ansContainer').html(dropTag);
   })
+
+
+
+ 
+  // function to generate the place value
+  function generatePlaceValue(){
+    let Arr = (Array.from($('#firstNo span')).reverse());
+    for(let i= 0; i <Arr.length; i++){
+       if(i==0){$(Arr[i]).append(`<span class='placeValue'>o</span>`);} 
+       if(i==1){$(Arr[i]).append(`<span class='placeValue'>t</span>`);} 
+       if(i==2){$(Arr[i]).append(`<span class='placeValue'>h</span>`);} 
+       if(i==3){$(Arr[i]).append(`<span class='placeValue'>th</span>`);} 
+       if(i==4){$(Arr[i]).append(`<span class='placeValue'>t.th</span>`);} 
+       if(i==5){$ (Arr[i]).append(`<span class='placeValue'>l</span>`);} 
+    }
+  }
+  generatePlaceValue()
+  // function to generate the place value end
+  
+
+  let Width = $('.drop').width();
+  $('#firstNo span').width(Width);
+  $('#secNo span').width(Width);
+
 
 });   // end document function 
